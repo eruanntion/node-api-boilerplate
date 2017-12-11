d74f8ccffd6316a18eac85a52ca337b15626f8ac
eruanntion <eruanntion@gmail.com>
2017-12-11 17:46:57 +0100

    reconfig

--- config/env.js
+++ config/env.js
@@ -1,4 +1,5 @@
 //region NPM module dependencies
+
 /**
  * Global config
  */
@@ -6,18 +7,24 @@ const nconf = require('nconf');
 const path = require('path');
 const fs = require('fs');
 const debug = require('debug')('node-api-boilerplate:config');
+
 //endregion
 
 //region If not provided, set environment to development
+
 const env = (process.env.NODE_ENV || 'development').trim();
+
 //endregion
 
 //region Load commandline arguments and environment variables, respectively
+
 nconf.argv().env();
 debug('Argv & env loaded');
+
 //endregion
 
 //region Load flag specific settings
+
 let flags = nconf.get('flag');
 
 if (flags) {
@@ -41,9 +48,11 @@ if (flags) {
 		debug(flag, 'config file loaded');
 	});
 }
+
 //endregion
 
 //region Load an environment configuration data into the hierarchy
+
 let env_file_path = `${__dirname}/environments/${env}.json`;
 
 if (!fs.existsSync(env_file_path)) {
@@ -55,9 +64,11 @@ if (!fs.existsSync(env_file_path)) {
 
 nconf.file(env, {file: env_file_path});
 debug(env, 'config file loaded');
+
 //endregion
 
 //region Load default configuration data into the hierarchy
+
 let default_file_path = `${__dirname}/environments/default.json`;
 
 if (!fs.existsSync(default_file_path)) {
@@ -68,22 +79,29 @@ if (!fs.existsSync(default_file_path)) {
 
 nconf.file('default', {file: default_file_path});
 debug('default config file loaded');
+
 //endregion
 
 //region Set custom variables
+
 nconf.set('root_dir', path.join(__dirname, '../'));
 debug('Custom variables loaded into config');
+
 //endregion
 
 //region Check if everything is loaded
+
 nconf.required(['env', 'http:port']);
+
 //endregion
 
 //region Bind get method to nconf, so you can use short notation
+
 /**
  * Global config
  */
 nconf.get = nconf.get.bind(nconf);
+
 //endregion
 
 module.exports = nconf;
--- config/express.js
+++ config/express.js
@@ -1,59 +1,86 @@
 //region NPM module dependencies
+
+const config = require('nconf');
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compress = require('compress');
+const methodOverride = require('method-override');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const debug = require('debug')('node-api-boilerplate:express');
+
 //endregion
 
 //region Developer's module dependencies
+
 const index = require('../routes/index');
 const users = require('../routes/users');
+
 //endregion
 
-//region Express instance
 /**
  * Express instance
  * @public
- * @type {Object}
  */
 const app = express();
-//endregion
 
-//region View engine setup
+//region Express middlewares setup
+
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-//endregion
 
-//region Express middleware setup
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// Request logging. dev: console | production: file
+app.use(logger(config('http:logs')));
+
+// Parse body params and attache them to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// gzip compression
+app.use(compress());
+
+// Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it
+app.use(methodOverride());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// Enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
 app.use(cookieParser());
+
+// Serve static files under public folder
 app.use(express.static(path.join(__dirname, 'public')));
-debug('Middleware setup finished');
+
 //endregion
 
-//region Routes loading
+//region Mount routes
+
+// Mount index route
 app.use('/', index);
+
+// Mount users route
 app.use('/users', users);
-debug('Routes loaded');
+
 //endregion
 
-//region Catch 404 and forward to error handler
+//region Error handling
+
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	const err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
-debug('404 catcher loaded');
-//endregion
 
-//region Error handler
+//Error handler
 app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
@@ -63,7 +90,7 @@ app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render('error');
 });
-debug('Express middleware error handler loaded');
+
 //endregion
 
 module.exports = app;
