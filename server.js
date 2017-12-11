cc51f739d53fe1341c7688743a3b8a2acf042398
eruanntion <eruanntion@gmail.com>
2017-12-11 20:04:20 +0100

    error handling

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const debug = require('debug')('node-api-boilerplate:server');
 
 const config = require('./config');
 const app = require('./config/express');
+const serverHandlers = require('./utils/serverHandlers');
 
 //endregion
 
@@ -14,35 +15,11 @@ const port = config('http:port');
 const server = http.createServer(app);
 
 app.set('port', port);
+
 server.listen(port);
-debug('HTTP server created');
 
-server.on('error', error => {
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
+server.on('error', serverHandlers.onError);
 
-	// handle specific listen errors with friendly messages
-	switch (error.code) {
-		case 'EACCES':
-			console.error(port + ' requires elevated privileges');
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error(port + ' is already in use');
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
-});
-
-server.on('listening', () => {
-	var addr = server.address();
-	var bind = typeof addr === 'string'
-		? 'pipe ' + addr
-		: 'port ' + addr.port;
-	console.log('Listening on ' + bind);
-});
+server.on('listening', serverHandlers.onListening);
 
 //endregion
---
+++ utils/serverHandlers.js
@@ -0,0 +1,34 @@
+/**
+ * on error event handler
+ * @public
+ * @param error
+ */
+exports.onError = error => {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case 'EACCES':
+			console.error(port + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(port + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+};
+
+/**
+ * on listening event handler
+ * @public
+ */
+exports.onListening = () => {
+	let address = server.address();
+	let bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + address.port;
+	console.log('Listening on ' + bind);
+};
